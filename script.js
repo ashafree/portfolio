850497b57a5b201240445f8e988152abdabc175d
ashafree <kima601@newschool.edu>
2020-05-06 16:40:38 -0400

    new

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-var clickMeButton = document.querySelector(".what");
 
-var clickWhat = document.querySelector(".what");
-var whatPicture = document.querySelector(".whatpic");
-
-clickWhat.addEventListener("click", function(){ whatPicture.classList.toggle("appear");
-})
-
-var clickMeButton = document.querySelector(".who");
-
-var clickWho = document.querySelector(".who");
-var whoPicture = document.querySelector(".whopic");
-
-clickWho.addEventListener("click", function(){ whoPicture.classList.toggle("appear");
-})
 
 
